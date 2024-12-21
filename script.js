70d887a80dfb2b4b5549353c164828f103293047
Abarca25 <190577751+Abarca25@users.noreply.github.com.>
2024-12-20 19:56:07 -0800

    Add event listener functionality and more styling

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 let game_choice = ["rock","paper","scissors"];
 let buttons = document.querySelectorAll("button");
+let player_score = document.querySelector("#player-score");
+let cpu_score = document.querySelector("#cpu-score");
+let game_container_target = document.querySelector(".game-container");
 
 function getComputerChoice(){
     let cpu_choice = Math.floor(Math.random() * 3);
@@ -8,22 +11,30 @@ function getComputerChoice(){
     return game_choice[cpu_choice];
 }
 
+function checkIfWinner (cpu_score,player_score){
+    return cpu_score <= 5 || player_score <= 5;
+}
+
 function compareChoices(cpu,player){
     let cpu_name = "Will Smith"
     let player_name = "Dummy"
 
-    if (cpu == "scissors" && player == "paper" || 
-        cpu == "rock" && player == "scissors" ||
-        cpu == "paper" && player == "rock"){
-            declareWinner(cpu_name);
-        }
-        else if (cpu == player){
-            declareTie();
-        }
-        else {
-            declareWinner(player_name);
-        }
+    if (checkIfWinner(Number(cpu_score.innerText),Number(player_score.innerText)) == true){
+        if (cpu == "scissors" && player == "paper" || 
+            cpu == "rock" && player == "scissors" ||
+            cpu == "paper" && player == "rock"){
+                trackOfScore(cpu_score,cpu_name)
+            }
+            else if (cpu == player){
+                declareTie();
+            }
+            else {
+                trackOfScore(player_score,player_name);
+            }
+    }
+
 }
+
 
 function declareWinner(winner){
     console.log(`${winner} wins this one!`)
@@ -33,9 +44,27 @@ function declareTie(){
     console.log("IT'S A TIE!")
 }
 
+function trackOfScore(winner,name) {
+    winner.innerText = Number(winner.innerText) + 1;
+    
+    if(Number(winner.innerText) >= 5){
+        let winnerName = document.createElement("div");
+        winnerName.setAttribute("class","winner");
+        winnerName.innerText = `${name} is the winner!`;
+        game_container_target.appendChild(winnerName)
+
+        buttons.forEach((button) => {
+            button.removeEventListener("click",handleEventClick);
+        });
+
+    }
+}
+
+function handleEventClick (event) {
+    compareChoices(getComputerChoice(), event.target.getAttribute("id"))
+}
+
 
 buttons.forEach((button) => {
-    button.addEventListener("click", () => {
-        compareChoices(getComputerChoice(), button.getAttribute("id"))
-    })
+    button.addEventListener("click",handleEventClick)
 })
