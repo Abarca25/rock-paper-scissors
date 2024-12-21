d59a48540d65468e193d076ab4c21d9f8a34c6a7
Abarca25 <190577751+Abarca25@users.noreply.github.com.>
2024-12-20 18:27:53 -0800

    Modify element flex and game logic.

--- script.js
+++ script.js
@@ -1,20 +1,11 @@
 let game_choice = ["rock","paper","scissors"];
+let buttons = document.querySelectorAll("button");
 
 function getComputerChoice(){
     let cpu_choice = Math.floor(Math.random() * 3);
 
     console.log(game_choice[cpu_choice])
     return game_choice[cpu_choice];
-}
-
-function getPlayerChoice(){
-    let player_choice = prompt("What weapon do you choose?");
-
-    while(!game_choice.includes(player_choice)){
-        player_choice = prompt("Choose a real weapon")
-    }
-
-    compareChoices(player_choice,getComputerChoice())
 }
 
 function compareChoices(cpu,player){
@@ -42,8 +33,9 @@ function declareTie(){
     console.log("IT'S A TIE!")
 }
 
-function gameLogic(){
-    getPlayerChoice();
-}
 
-// gameLogic();
+buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+        compareChoices(getComputerChoice(), button.getAttribute("id"))
+    })
+})
